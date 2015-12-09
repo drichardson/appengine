0085b155743e78d429002ad763189dd509bbcb1c
Doug Richardson <dougie.richardson@gmail.com>
2015-12-08 22:20:49 -0800

    ~ doesn't work here

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,7 +8,7 @@ curl https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz| t
 # Answer all the gcloud installation questions so there aren't any prompts.
 # Install beta components, which, at this time, contain the dev_appserver.py needed
 # by google.golang.org/appengine/aetest which is used in some of the tests.
-"~/google-cloud-sdk/install.sh" \
+"$HOME/google-cloud-sdk/install.sh" \
     --usage-reporting true \
     --additional-components beta \
     --path-update false \
