f7ac4edd3814a7a2aca532be1129dbc388390729
Doug Richardson <dougie.richardson@gmail.com>
2015-12-08 22:23:30 -0800

    try app-engine-python component instead of beta

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,10 +6,10 @@ set -e
 curl https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz| tar -C ~ -x -z -f -
 
 # Answer all the gcloud installation questions so there aren't any prompts.
-# Install beta components, which, at this time, contain the dev_appserver.py needed
+# Install components, which, at this time, contain the dev_appserver.py needed
 # by google.golang.org/appengine/aetest which is used in some of the tests.
 "$HOME/google-cloud-sdk/install.sh" \
     --usage-reporting true \
-    --additional-components beta \
+    --additional-components app-engine-python \
     --path-update false \
     --command-completion false
